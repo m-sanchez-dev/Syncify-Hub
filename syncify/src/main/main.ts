afb78070f149906d99c9d5783d55168d2e738853
Mikel Sanchez <mikelsanchezmart@gmail.com>
2023-11-22 22:40:31 +0000

    refacto

--- syncify/src/main/main.ts
+++ syncify/src/main/main.ts
@@ -3,35 +3,23 @@ import * as path from 'path';
 
 let mainWindow: BrowserWindow | null;
 
-class AppDelegate {
-  // Add this method to address the warning
-  applicationSupportsSecureRestorableState(): boolean {
-    return true;
-  }
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-  // ... Rest of your Electron app code ...
+  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
 
-  // For example:
-  createWindow() {
-    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true,
-      },
-    });
-
-    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
-
-    mainWindow.on('closed', () => {
-      mainWindow = null;
-    });
-  }
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-const myApp = new AppDelegate();
-
-app.whenReady().then(myApp.createWindow);
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -41,6 +29,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    myApp.createWindow();
+    createWindow();
   }
 });
